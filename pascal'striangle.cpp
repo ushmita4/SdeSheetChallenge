939b61aeaa4bc8b9fddc486146cce984baa83dd5
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2022-06-07 21:52:50 +0530

    Create pascal'striangle.cpp

---
+++ pascal'striangle.cpp
@@ -0,0 +1,13 @@
+vector<vector<long long int>> printPascal(int n) 
+{
+ vector<vector<long long int>> r(n);
+    for(long long int i=0;i<n;i++){
+        r[i].resize(i+1);
+        r[i][0]=r[i][i]=1;
+        
+        
+        for (long long int j = 1; j < i; j++)
+                r[i][j] = r[i - 1][j - 1] + r[i - 1][j];
+    }
+    return r;
+}
