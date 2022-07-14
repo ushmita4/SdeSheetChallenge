f9ccc1fe64c25ece74fa92e3c893ef09c1e08a52
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2022-07-14 19:51:51 +0530

    Create bellmanford.cpp

---
+++ bellmanford.cpp
@@ -0,0 +1,26 @@
+int bellmonFord(int n, int m, int src, int dest, vector<vector<int>> &edges) {
+    
+ 
+    vector<int> d(n + 1, 1e9);
+   
+    d[src] = 0;
+
+ 
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            int u = edges[j][0];
+            int v = edges[j][1];
+            int w = edges[j][2];
+
+            if (d[u] != 1e9 && d[v] > (d[u] + w)) {
+                d[v] = d[u] + w;
+            }
+        }
+    }
+
+    
+    return d[dest];
+    
+}
