efea55a56c427700a97559da7d6073126d180eb0
Ushmita Dutta <76655696+ushmita4@users.noreply.github.com>
2022-07-28 08:59:16 +0530

    Create maximumsumincreasingsubsequence.cpp

---
+++ maximumsumincreasingsubsequence.cpp
@@ -0,0 +1,27 @@
+int maxIncreasingDumbbellsSum(vector<int> &rack, int n)
+{
+    int dp[n];
+    int totalWeight = INT_MIN;
+
+    for(int i = 0; i < n; i++)
+    {
+        dp[i] = rack[i];
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < i; j++)
+        {
+            
+            if(rack[i] > rack[j] && dp[i] < dp[j] + rack[i])
+            {
+                dp[i] = dp[j] + rack[i];
+            }
+        }
+
+        
+        totalWeight = max(totalWeight, dp[i]);
+    }
+
+    return totalWeight;
+}
